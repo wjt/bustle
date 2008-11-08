bf763e64e6a2c2e9c0dfffc785f58c7ae8e0e4b9
Will Thompson <will.thompson@collabora.co.uk>
2008-11-08 17:50:31 +0000

    draw something resembling a curve from returns to their call

--- Bustle.hs
+++ Bustle.hs
@@ -27,12 +27,19 @@ import Data.Set (Set)
 import qualified Data.Map as Map
 import Data.Map (Map)
 
+import Data.List (stripPrefix)
+import Data.Maybe (fromMaybe)
+
 import Control.Monad.State
 import Control.Monad (forM_)
 
 import Graphics.UI.Gtk hiding (get)
 import Graphics.Rendering.Cairo
 import Control.Monad.Trans ( liftIO )
+
+import Debug.Trace (trace)
+
+traceM x = trace (show x) (return ())
 
 main = do
     input <- readFile "/tmp/face"
@@ -53,12 +60,18 @@ Plan:
 
 data BustleState =
     BustleState { coordinates :: Map BusName Double
-                , pending :: Map (BusName, Serial) (Message, Double)
+                , pending :: Map (BusName, Serial) (Message, (Double, Double))
                 , row :: Double
                 , mostRecentLabels :: Double
                 }
 
 modifyCoordinates f = modify (\bs -> bs { coordinates = f (coordinates bs)})
+
+addPending m x = do
+    y <- gets row
+    let update = Map.insert (sender m, serial m) (m, (x, y))
+
+    modify $ \bs -> bs { pending = update (pending bs) }
 
 advanceBy :: Double -> StateT BustleState Render ()
 advanceBy d = do
@@ -94,19 +107,18 @@ lastComponent :: BusName -> String
 lastComponent = reverse . takeWhile (/= '.') . reverse
 
 drawHeader :: BusName -> Double -> Double -> Render ()
-drawHeader name x y = do
+drawHeader name' x y = do
+    let name = abbreviate name'
     extents <- textExtents name
     let diff = textExtentsWidth extents / 2
     moveTo (x - diff) (y + 10)
     showText name
 
-
 addApplication :: BusName -> Double -> StateT BustleState Render Double
 addApplication s c = do
-    let name = s --lastComponent s
     currentRow <- gets row
 
-    lift $ do drawHeader name c (currentRow - 20)
+    lift $ do drawHeader s c (currentRow - 20)
 
               setSourceRGB 0.7 0.7 0.7
               setLineWidth 1
@@ -121,7 +133,7 @@ addApplication s c = do
     modifyCoordinates (Map.insert s c)
     return c
 
-firstAppX = 100
+firstAppX = 150
 
 appCoordinate :: BusName -> StateT BustleState Render Double
 appCoordinate s = do
@@ -137,14 +149,72 @@ senderCoordinate m = appCoordinate (sender m)
 destinationCoordinate :: Message -> StateT BustleState Render Double
 destinationCoordinate m = appCoordinate (destination m)
 
+abbreviate :: Interface -> Interface
+abbreviate i = fromMaybe i $ stripPrefix "org.freedesktop." i
+
+memberName :: Message -> StateT BustleState Render ()
+memberName m = do
+    current <- gets row
+
+    lift $ do
+        moveTo 0 current
+        showText $ path m
+
+        moveTo 0 (current + 10)
+        showText . abbreviate $ iface m ++ " . " ++ member m
+
+
+returnArc m currentx = do
+    ps <- gets pending
+    currenty <- gets row
+
+    case Map.lookup (destination m, inReplyTo m) ps of
+        Nothing -> return ()
+        Just (m, (callx, cally)) -> lift $ do
+            let vdiff = (currenty - cally) / 3
+                curvey1 = currenty - vdiff
+                curvey2 = currenty - vdiff * 2
+
+                direction = if callx > currentx then (+) else (-)
+
+                curvex = (max currentx callx) `direction` 30
+
+            setSourceRGB 0.4 0.7 0.4
+            setDash [3, 3] 0
+
+            moveTo currentx currenty
+            lineTo curvex curvey1
+            lineTo curvex curvey2
+            lineTo callx cally
+--            curveTo curvex curvey1 curvex curvey2 callx cally
+            stroke
+
+            setDash [] 0
+            -- XXX Remove from pending
+
 munge :: Message -> StateT BustleState Render ()
 munge m = do
     advanceBy 30 -- FIXME: use some function of timestamp
+
     sc <- senderCoordinate m
     case m of
-        Signal {} -> signal sc
-        MethodCall {}   -> destinationCoordinate m >>= methodCall sc
-        MethodReturn {} -> destinationCoordinate m >>= methodReturn sc
+        Signal {}       -> memberName m >> signal sc
+
+        MethodCall {}   -> do
+            memberName m
+
+            dc <- destinationCoordinate m
+            methodCall sc dc
+
+            addPending m dc
+
+        MethodReturn {} -> do
+            dc <- destinationCoordinate m
+
+            methodReturn sc dc
+
+            returnArc m sc
+
         Error {}        -> error "eh"
 
 process' :: [Message] -> StateT BustleState Render ()
