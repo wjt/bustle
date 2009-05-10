wjt/bustle

commit fd5b7c5756710f98567a7dd0ab2df2031422b22f

Prepare UI for process returning an error

Bustle.hs
@@ -186,7 +186,9 @@ loadLogWith act f = do
   where llw' = do
           input <- etio show $ readFile f
           log <- toET (("Parse error " ++) . show) $ readLog input
-          lift (act >>= \misc -> displayLog misc f (upgrade log))
+          misc <- lift act
+          shapes <- toET id $ process (upgrade log)
+          lift (displayLog misc f shapes)
 
 
 maybeQuit :: B ()
@@ -231,10 +233,9 @@ emptyWindow = do
   incWindows
   return (window, saveItem, layout)
 
-displayLog :: WindowInfo -> FilePath -> [Message] -> B ()
-displayLog (window, saveItem, layout) filename log = do
-  let shapes = process log
-      (width, height) = dimensions shapes
+displayLog :: WindowInfo -> FilePath -> Diagram -> B ()
+displayLog (window, saveItem, layout) filename shapes = do
+  let (width, height) = dimensions shapes
       details = (filename, shapes)
 
   io $ do

Bustle/Renderer.hs
@@ -42,8 +42,8 @@ import Data.List (isPrefixOf, stripPrefix, sortBy)
 import Data.Maybe (fromMaybe, catMaybes)
 import Data.Ord (comparing)
 
-process :: [Message] -> [Shape]
-process log = execRenderer (mapM_ munge log') (initialState initTime)
+process :: [Message] -> Either String [Shape]
+process log = Right $ execRenderer (mapM_ munge log') (initialState initTime)
 
   where -- FIXME: really? Maybe we should allow people to be interested in,
         --        say, binding to signals?

commit 694b02434ae771161d81cb66761d9f8291ebac0e

Fill in the empty window when 'process' fails.
I broke this in fd5b7c5756710f98567a7dd0ab2df2031422b22f

Bustle.hs
@@ -186,8 +186,8 @@ loadLogWith act f = do
   where llw' = do
           input <- etio show $ readFile f
           log <- toET (("Parse error " ++) . show) $ readLog input
-          misc <- lift act
           shapes <- toET id $ process (upgrade log)
+          misc <- lift act
           lift (displayLog misc f shapes)
 
 