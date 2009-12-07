41ca6279714c45a39fdab4073d29d1278c520462
Will Thompson <will.thompson@collabora.co.uk>
2009-12-07 01:26:29 +0000

    Extract guts of bustle-time to Bustle.Stats

--- Bustle/Stats.hs
+++ Bustle/Stats.hs
@@ -1,11 +1,14 @@
 module Bustle.Stats
   ( TallyType
   , frequencies
+  , methodTimes
   )
 where
 
-import Data.List (sort)
+import Data.List (sort, sortBy)
 import Data.Maybe (mapMaybe)
+import Data.Ord (comparing)
+
 import qualified Data.Map as M
 
 import Bustle.Types
@@ -13,13 +16,15 @@ import Bustle.Types
 data TallyType = TallyMethod | TallySignal
     deriving (Eq, Ord, Show)
 
+memberStr :: Member -> String
+memberStr m = iface m ++ "." ++ membername m
+
 repr :: Message -> Maybe (TallyType, String)
 repr msg =
     case msg of
         MethodCall { member = m } -> Just (TallyMethod, memberStr m)
         Signal     { member = m } -> Just (TallySignal, memberStr m)
         _                         -> Nothing
-    where memberStr m = iface m ++ "." ++ membername m
 
 frequencies :: Log -> [(Int, (TallyType, String))]
 frequencies = reverse
@@ -30,3 +35,31 @@ frequencies = reverse
             . mapMaybe repr
     where alt Nothing  = Just 1
           alt (Just n) = Just (n + 1)
+
+mean :: Fractional a => [a] -> a
+mean = acc 0 0
+   where acc 0 _ [] = error "mean of empty list"
+         acc n t [] = t / n
+         acc n t (x:xs) = acc (n + 1) (t + x) xs
+
+methodTimes :: Log -> [(String, Double, Int, Double)]
+methodTimes = reverse
+            . sortBy (comparing (\(_, b, _, _) -> b))
+            . map summarize
+            . M.toList
+            . foldr (\(method, time) -> M.alter (alt time) method) M.empty
+            . mapMaybe methodReturn
+    where alt newtime Nothing = Just (newtime, [newtime])
+          alt newtime (Just (total, times)) =
+              Just (newtime + total, newtime : times)
+
+          methodReturn (MethodReturn { timestamp = end,
+                            inReplyTo = Just (MethodCall {
+                                timestamp = start, member = m }) }) =
+              Just (memberStr m, end - start)
+          methodReturn _ = Nothing
+
+          summarize (method, (total, times)) = (method,
+                         fromInteger total / 1000,
+                         length times,
+                         (mean $ map fromInteger times) / 1000)
--- bustle-time.hs
+++ bustle-time.hs
@@ -1,41 +1,11 @@
 import Prelude hiding (log)
 
-import Data.Function
-import qualified Data.Map as M
-import Data.List
-import Data.Maybe
 import System
 import Text.Printf
 
 import Bustle.Parser (readLog)
 import Bustle.Types
-
-mean :: Fractional a => [a] -> a
-mean = acc 0 0
-   where acc 0 _ [] = error "mean of empty list"
-         acc n t [] = t / n
-         acc n t (x:xs) = acc (n + 1) (t + x) xs
-
-mkTimes :: [Message] -> [(String, Double, Int, Double)]
-mkTimes = map (\(method, (total, times)) ->
-                  (method,
-                   fromInteger total / 1000,
-                   length times,
-                   (mean $ map fromInteger times) / 1000))
-      . M.toList
-      . foldr (\(method, time) -> M.alter (alt time) method) M.empty
-      . mapMaybe methodReturn
-    where alt newtime Nothing = Just (newtime, [newtime])
-          alt newtime (Just (total, times)) =
-              Just (newtime + total, newtime : times)
-
-          methodReturn (MethodReturn { timestamp = end,
-                            inReplyTo = Just (MethodCall {
-                                timestamp = start, member = m }) }) =
-              Just (memberStr m, end - start)
-          methodReturn _ = Nothing
-
-          memberStr m = iface m ++ "." ++ membername m
+import Bustle.Stats (methodTimes)
 
 run :: FilePath -> IO ()
 run filepath = do
@@ -45,9 +15,7 @@ run filepath = do
         Right log -> mapM_ (\(method, total, ncalls, mean_) ->
                                  printf " %9.4f %3d %9.4f %s\n"
                                      total ncalls mean_ method)
-                         . reverse
-                         . sortBy (compare `on` (\(_, b, _, _) -> b))
-                         . mkTimes $ log
+                         . methodTimes $ log
 
 main :: IO ()
 main = do
