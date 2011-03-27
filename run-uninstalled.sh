7d724e3a32444e137fad03d43c444f6e3dba0a5f
Will Thompson <will.thompson@collabora.co.uk>
2011-03-27 21:16:34 +0100

    run-uninstalled: check that Bustle is built.

--- run-uninstalled.sh
+++ run-uninstalled.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
+set -e
 
 bustle_datadir="$(dirname ${0}})"
 export bustle_datadir
 
 bustle="${bustle_datadir}"/dist/build/bustle/bustle
 
-if [ -e $bustle ]; then
-    exec $bustle "${@}"
-else
-    echo "Bustle hasn't been built yet; consult the README"
-    exit 1
-fi
+cabal build
+exec $bustle "${@}"
