ebf0ce92f562fefbc9623433cee0f8b254c232f4
Will Thompson <will@willthompson.co.uk>
2011-08-04 10:16:06 +0100

    Add instructions to the filtering dialog

--- Bustle/UI/FilterDialog.hs
+++ Bustle/UI/FilterDialog.hs
@@ -21,27 +21,19 @@ formatNames (u, os)
     | Set.null os = unUniqueName u
     | otherwise = intercalate "\n" . map unOtherName $ Set.toAscList os
 
+type NameStore = ListStore (Bool, (UniqueName, Set OtherName))
+
 makeStore :: [(UniqueName, Set OtherName)]
           -> Set UniqueName
-          -> IO (ListStore (Bool, (UniqueName, Set OtherName)))
+          -> IO NameStore
 makeStore names currentlyHidden = do
     listStoreNew $ map toPair names
   where
     toPair (name@(u, _)) = (not (Set.member u currentlyHidden), name)
 
-runFilterDialog :: WindowClass parent
-                => parent
-                -> [(UniqueName, Set OtherName)]
-                -> Set UniqueName
-                -> IO (Set UniqueName) -- ^ The set of names to *hide*
-runFilterDialog parent names currentlyHidden = do
-    d <- dialogNew
-    windowSetTransientFor d parent
-
-    dialogAddButton d stockClose ResponseClose
-
-    nameStore <- makeStore names currentlyHidden
-
+makeView :: NameStore
+         -> IO ScrolledWindow
+makeView nameStore = do
     nameView <- treeViewNewWithModel nameStore
     -- We want rules because otherwise it's tough to see where each group
     -- starts and ends
@@ -74,9 +66,35 @@ runFilterDialog parent names currentlyHidden = do
     scrolledWindowSetPolicy sw PolicyAutomatic PolicyAutomatic
     containerAdd sw nameView
 
+    return sw
+
+runFilterDialog :: WindowClass parent
+                => parent -- ^ The window to which to attach the dialog
+                -> [(UniqueName, Set OtherName)] -- ^ Names, in order of appearance
+                -> Set UniqueName -- ^ Currently-hidden names
+                -> IO (Set UniqueName) -- ^ The set of names to *hide*
+runFilterDialog parent names currentlyHidden = do
+    d <- dialogNew
+    windowSetTransientFor d parent
+    dialogAddButton d stockClose ResponseClose
     vbox <- dialogGetUpper d
+    boxSetSpacing vbox 6
+
+    nameStore <- makeStore names currentlyHidden
+    sw <- makeView nameStore
+
+    instructions <- labelNew Nothing
+    widgetSetSizeRequest instructions 600 (-1)
+    labelSetMarkup instructions
+        "Unticking a service hides its column in the diagram, \
+        \and all messages it is involved in. That is, all methods it calls \
+        \or are called on it, the corresponding returns, and all signals it \
+        \emits will be hidden."
+    labelSetLineWrap instructions True
+    boxPackStart vbox instructions PackNatural 0
+
     containerAdd vbox sw
-    widgetShowAll sw
+    widgetShowAll vbox
 
     _ <- dialogRun d
 
