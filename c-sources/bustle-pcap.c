23438e5a8b0c092817229e1512ca4f6f5163267b
Will Thompson <will@willthompson.co.uk>
2012-01-11 11:03:05 +0000

    bustle-pcap: librarify guts

--- c-sources/bustle-pcap.c
+++ c-sources/bustle-pcap.c
@@ -17,108 +17,16 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include <stdlib.h>
 #include <string.h>
-#include <pcap/pcap.h>
 #include <glib.h>
 #include <glib-unix.h>
 #include <glib/gprintf.h>
-#include <gio/gio.h>
 #include <gio/gunixinputstream.h>
 
-#define DIE_IF_NULL(_x, _fmt, ...) \
-  G_STMT_START { \
-    if (_x == NULL) \
-      { \
-        fprintf (stderr, _fmt "\n", ##__VA_ARGS__); \
-        exit (1); \
-      } \
-  } G_STMT_END
+#include "pcap.h"
 
-GDBusCapabilityFlags caps;
 static gboolean verbose = FALSE;
 static gboolean quiet = FALSE;
-
-/* Not using GString because it holds signed chars but we both receive and
- * provide unsigned chars. C!
- */
-typedef struct {
-    struct timeval ts;
-    gsize size;
-    guchar *blob;
-} Message;
-
-#define STOP ((Message *) 0x1)
-
-typedef struct {
-    pcap_dumper_t *dumper;
-    GAsyncQueue *message_queue;
-} ThreadData;
-
-static gpointer
-log_thread (gpointer data)
-{
-  ThreadData *td = data;
-  Message *message;
-
-  while (STOP != (message = g_async_queue_pop (td->message_queue)))
-    {
-      struct pcap_pkthdr hdr;
-      hdr.ts = message->ts;
-
-      hdr.caplen = message->size;
-      hdr.len = message->size;
-
-      /* The cast is necessary because libpcap is weird. */
-      pcap_dump ((u_char *) td->dumper, &hdr, message->blob);
-      g_free (message->blob);
-      g_slice_free (Message, message);
-    }
-
-  return NULL;
-}
-
-GDBusMessage *
-filter (
-    GDBusConnection *connection,
-    GDBusMessage *message,
-    gboolean is_incoming,
-    gpointer user_data)
-{
-  GAsyncQueue *message_queue = user_data;
-  const gchar *dest;
-  Message m;
-  GError *error = NULL;
-
-  gettimeofday (&m.ts, NULL);
-  m.blob = g_dbus_message_to_blob (message, &m.size, caps, &error);
-  DIE_IF_NULL (m.blob, "Couldn't marshal message: %s", error->message);
-  g_async_queue_push (message_queue, g_slice_dup (Message, &m));
-
-  dest = g_dbus_message_get_destination (message);
-
-  if (verbose)
-    g_print ("(%s) %s -> %s: %u %s\n",
-        is_incoming ? "incoming" : "outgoing",
-        g_dbus_message_get_sender (message),
-        dest,
-        g_dbus_message_get_message_type (message),
-        g_dbus_message_get_member (message));
-
-  if (!is_incoming ||
-      g_strcmp0 (dest, g_dbus_connection_get_unique_name (connection)) == 0)
-    {
-      /* This message is either outgoing or actually for us, as opposed to
-       * being eavesdropped. */
-      return message;
-    }
-
-  /* We have to say we've handled the message, or else GDBus replies to other
-   * people's method calls and we all get really confused.
-   */
-  g_object_unref (message);
-  return NULL;
-}
 
 #if GLIB_CHECK_VERSION (2, 30, 0)
 static void
@@ -159,69 +67,6 @@ let_me_quit (GMainLoop *loop)
   g_printf ("Hit Enter to stop logging. (Do not hit Control-C.)\n");
 }
 #endif
-
-static void
-match_everything (GDBusProxy *bus)
-{
-  char *rules[] = {
-      "type='signal'",
-      "type='method_call'",
-      "type='method_return'",
-      "type='error'",
-      NULL
-  };
-  char **r;
-  GError *error = NULL;
-
-  for (r = rules; *r != NULL; r++)
-    {
-      GVariant *ret = g_dbus_proxy_call_sync (
-          bus,
-          "AddMatch",
-          g_variant_new ("(s)", *r),
-          G_DBUS_CALL_FLAGS_NONE,
-          -1,
-          NULL,
-          &error);
-      DIE_IF_NULL (ret, "Couldn't AddMatch(%s): %s", *r, error->message);
-    }
-}
-
-static void
-list_all_names (
-    GDBusProxy *bus)
-{
-  GError *error = NULL;
-  GVariant *ret;
-  gchar **names;
-
-  g_assert (G_IS_DBUS_PROXY (bus));
-
-  ret = g_dbus_proxy_call_sync (bus, "ListNames", NULL,
-      G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
-  DIE_IF_NULL (ret, "Couldn't ListNames: %s", error->message);
-
-  for (g_variant_get_child (ret, 0, "^a&s", &names);
-       *names != NULL;
-       names++)
-    {
-      gchar *name = *names;
-
-      if (!g_dbus_is_unique_name (name) &&
-          strcmp (name, "org.freedesktop.DBus") != 0)
-        {
-          GVariant *owner = g_dbus_proxy_call_sync (bus, "GetNameOwner",
-              g_variant_new ("(s)", name),
-              G_DBUS_CALL_FLAGS_NONE, -1, NULL, NULL);
-
-          if (owner != NULL)
-            g_variant_unref (owner);
-          /* else they were too quick for us! */
-        }
-    }
-
-  g_variant_unref (ret);
-}
 
 static gboolean session_specified = FALSE;
 static gboolean system_specified = FALSE;
@@ -306,58 +151,18 @@ main (
   GMainLoop *loop;
   GBusType bus_type;
   gchar *filename;
-  GDBusConnection *connection;
-  GDBusProxy *bus;
-  GThread *thread;
-  ThreadData td;
-  pcap_t *p;
-  guint filter_id;
   GError *error = NULL;
+  BustlePcap *pcap;
 
   g_type_init ();
   parse_arguments (&argc, &argv, &bus_type, &filename);
 
-  /* FIXME: use DLT_DBUS when it makes it into libpcap. */
-  p = pcap_open_dead (DLT_NULL, 1 << 27);
-  DIE_IF_NULL (p, "pcap_open_dead failed. wtf");
-
-  td.dumper = pcap_dump_open (p, filename);
-  DIE_IF_NULL (td.dumper, "Couldn't open pcap dump: %s", pcap_geterr (p));
-
-  td.message_queue = g_async_queue_new ();
-
-  thread = g_thread_create (log_thread, &td, TRUE, &error);
-  DIE_IF_NULL (thread, "Couldn't spawn logging thread: %s", error->message);
-
-  connection = g_bus_get_sync (bus_type, NULL, &error);
-  DIE_IF_NULL (connection, "Couldn't connect to %s bus: %s",
-      bus_type == G_BUS_TYPE_SESSION ? "session" : "system",
-      error->message);
-
-  caps = g_dbus_connection_get_capabilities (connection);
-  bus = g_dbus_proxy_new_sync (connection,
-      G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES |
-      G_DBUS_PROXY_FLAGS_DO_NOT_CONNECT_SIGNALS,
-      NULL,
-      "org.freedesktop.DBus",
-      "/org/freedesktop/DBus",
-      "org.freedesktop.DBus",
-      NULL,
-      &error);
-  DIE_IF_NULL (bus, "Couldn't construct bus proxy: %s", error->message);
-  match_everything (bus);
-
-  filter_id = g_dbus_connection_add_filter (connection, filter,
-      g_async_queue_ref (td.message_queue),
-      (GDestroyNotify) g_async_queue_unref);
-
-  /* FIXME: there's a race between listing all the names and binding to all
-   * signals (and hence getting NameOwnerChanged). Old bustle-dbus-monitor had
-   * it too.
-   */
-  list_all_names (bus);
-
-  g_object_unref (bus);
+  pcap = bustle_pcap_new (bus_type, filename, verbose, &error);
+  if (pcap == NULL)
+    {
+      fprintf (stderr, "%s", error->message);
+      exit (1);
+    }
 
   loop = g_main_loop_new (NULL, FALSE);
 
@@ -368,14 +173,8 @@ main (
   g_main_loop_run (loop);
   g_main_loop_unref (loop);
 
-  g_dbus_connection_remove_filter (connection, filter_id);
-  g_dbus_connection_close_sync (connection, NULL, NULL);
-
-  /* Wait for the writer thread to spit out all the messages, then close up. */
-  g_async_queue_push (td.message_queue, STOP);
-  g_thread_join (thread);
-  pcap_dump_close (td.dumper);
-  pcap_close (p);
+  bustle_pcap_stop (pcap);
+  g_object_unref (pcap);
 
   return 0;
 }
---
+++ c-sources/pcap.c
@@ -0,0 +1,500 @@
+/*
+ * pcap.c - blah blah
+ * Copyright ©2011 Collabora Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "pcap.h"
+
+#include <string.h>
+#include <pcap/pcap.h>
+
+/* Not using GString because it holds signed chars but we both receive and
+ * provide unsigned chars. C!
+ */
+typedef struct {
+    struct timeval ts;
+    gsize size;
+    guchar *blob;
+} Message;
+
+#define STOP ((Message *) 0x1)
+
+typedef struct {
+    pcap_dumper_t *dumper;
+    GAsyncQueue *message_queue;
+} ThreadData;
+
+struct _BustlePcapPrivate {
+    GBusType bus_type;
+    GDBusConnection *connection;
+    GDBusCapabilityFlags caps;
+
+    guint filter_id;
+
+    gchar *filename;
+    pcap_t *p;
+
+    GThread *thread;
+    ThreadData td;
+
+    /* FIXME: this does not really belong here. */
+    gboolean verbose;
+};
+
+enum {
+    PROP_BUS_TYPE = 1,
+    PROP_FILENAME,
+    PROP_VERBOSE,
+};
+
+static void initable_iface_init (
+    gpointer g_class,
+    gpointer unused);
+
+G_DEFINE_TYPE_WITH_CODE (BustlePcap, bustle_pcap, G_TYPE_OBJECT,
+    G_IMPLEMENT_INTERFACE (G_TYPE_INITABLE, initable_iface_init);
+    )
+
+static void
+bustle_pcap_init (BustlePcap *self)
+{
+  self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self, BUSTLE_TYPE_PCAP,
+      BustlePcapPrivate);
+  self->priv->bus_type = G_BUS_TYPE_SESSION;
+  self->priv->td.message_queue = g_async_queue_new ();
+}
+
+static void
+bustle_pcap_get_property (
+    GObject *object,
+    guint property_id,
+    GValue *value,
+    GParamSpec *pspec)
+{
+  BustlePcap *self = BUSTLE_PCAP (object);
+  BustlePcapPrivate *priv = self->priv;
+
+  switch (property_id)
+    {
+      case PROP_BUS_TYPE:
+        g_value_set_enum (value, priv->bus_type);
+        break;
+      case PROP_FILENAME:
+        g_value_set_string (value, priv->filename);
+        break;
+      case PROP_VERBOSE:
+        g_value_set_boolean (value, priv->verbose);
+        break;
+      default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    }
+}
+
+static void
+bustle_pcap_set_property (
+    GObject *object,
+    guint property_id,
+    const GValue *value,
+    GParamSpec *pspec)
+{
+  BustlePcap *self = BUSTLE_PCAP (object);
+  BustlePcapPrivate *priv = self->priv;
+
+  switch (property_id)
+    {
+      case PROP_BUS_TYPE:
+        priv->bus_type = g_value_get_enum (value);
+        break;
+      case PROP_FILENAME:
+        priv->filename = g_value_dup_string (value);
+        break;
+      case PROP_VERBOSE:
+        priv->verbose = g_value_get_boolean (value);
+        break;
+      default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    }
+}
+
+static void
+bustle_pcap_dispose (GObject *object)
+{
+  BustlePcap *self = BUSTLE_PCAP (object);
+  BustlePcapPrivate *priv = self->priv;
+  GObjectClass *parent_class = bustle_pcap_parent_class;
+
+  if (parent_class->dispose != NULL)
+    parent_class->dispose (object);
+
+  /* Make sure we're all closed up. */
+  bustle_pcap_stop (self);
+
+  g_clear_object (&priv->connection);
+}
+
+static void
+bustle_pcap_finalize (GObject *object)
+{
+  BustlePcap *self = BUSTLE_PCAP (object);
+  BustlePcapPrivate *priv = self->priv;
+  GObjectClass *parent_class = bustle_pcap_parent_class;
+
+  if (parent_class->finalize != NULL)
+    parent_class->finalize (object);
+
+  g_free (priv->filename);
+  priv->filename = NULL;
+
+  g_async_queue_unref (priv->td.message_queue);
+  priv->td.message_queue = NULL;
+}
+
+static void
+bustle_pcap_class_init (BustlePcapClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  GParamSpec *param_spec;
+
+  object_class->get_property = bustle_pcap_get_property;
+  object_class->set_property = bustle_pcap_set_property;
+  object_class->dispose = bustle_pcap_dispose;
+  object_class->finalize = bustle_pcap_finalize;
+
+  g_type_class_add_private (klass, sizeof (BustlePcapPrivate));
+
+#define THRICE(x) x, x, x
+
+  param_spec = g_param_spec_enum (THRICE ("bus-type"),
+      G_TYPE_BUS_TYPE, G_BUS_TYPE_SESSION,
+      G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+  g_object_class_install_property (object_class, PROP_BUS_TYPE, param_spec);
+
+  param_spec = g_param_spec_string (THRICE ("filename"), NULL,
+      G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+  g_object_class_install_property (object_class, PROP_FILENAME, param_spec);
+
+  param_spec = g_param_spec_boolean (THRICE ("verbose"), FALSE,
+      G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+  g_object_class_install_property (object_class, PROP_VERBOSE, param_spec);
+}
+
+static gpointer
+log_thread (gpointer data)
+{
+  ThreadData *td = data;
+  Message *message;
+
+  while (STOP != (message = g_async_queue_pop (td->message_queue)))
+    {
+      struct pcap_pkthdr hdr;
+      hdr.ts = message->ts;
+
+      hdr.caplen = message->size;
+      hdr.len = message->size;
+
+      /* The cast is necessary because libpcap is weird. */
+      pcap_dump ((u_char *) td->dumper, &hdr, message->blob);
+      g_free (message->blob);
+      g_slice_free (Message, message);
+    }
+
+  return NULL;
+}
+
+GDBusMessage *
+filter (
+    GDBusConnection *connection,
+    GDBusMessage *message,
+    gboolean is_incoming,
+    gpointer user_data)
+{
+  BustlePcap *self = BUSTLE_PCAP (user_data);
+  const gchar *dest;
+  Message m;
+  GError *error = NULL;
+
+  gettimeofday (&m.ts, NULL);
+  m.blob = g_dbus_message_to_blob (message, &m.size, self->priv->caps, &error);
+  if (m.blob == NULL)
+    {
+      g_critical ("Couldn't marshal message: %s", error->message);
+      g_return_val_if_reached (NULL);
+    }
+  g_async_queue_push (self->priv->td.message_queue, g_slice_dup (Message, &m));
+
+  dest = g_dbus_message_get_destination (message);
+
+  if (self->priv->verbose)
+    g_print ("(%s) %s -> %s: %u %s\n",
+        is_incoming ? "incoming" : "outgoing",
+        g_dbus_message_get_sender (message),
+        dest,
+        g_dbus_message_get_message_type (message),
+        g_dbus_message_get_member (message));
+
+  if (!is_incoming ||
+      g_strcmp0 (dest, g_dbus_connection_get_unique_name (connection)) == 0)
+    {
+      /* This message is either outgoing or actually for us, as opposed to
+       * being eavesdropped. */
+      return message;
+    }
+
+  /* We have to say we've handled the message, or else GDBus replies to other
+   * people's method calls and we all get really confused.
+   */
+  g_object_unref (message);
+  return NULL;
+}
+
+static gboolean
+match_everything (
+    GDBusProxy *bus,
+    GError **error)
+{
+  char *rules[] = {
+      "type='signal'",
+      "type='method_call'",
+      "type='method_return'",
+      "type='error'",
+      NULL
+  };
+  char **r;
+
+  for (r = rules; *r != NULL; r++)
+    {
+      GVariant *ret = g_dbus_proxy_call_sync (
+          bus,
+          "AddMatch",
+          g_variant_new ("(s)", *r),
+          G_DBUS_CALL_FLAGS_NONE,
+          -1,
+          NULL,
+          error);
+
+      if (ret == NULL)
+        {
+          g_prefix_error (error, "Couldn't AddMatch(%s): ", *r);
+          return FALSE;
+        }
+      else
+        {
+          g_variant_unref (ret);
+        }
+    }
+
+  return TRUE;
+}
+
+static gboolean
+list_all_names (
+    GDBusProxy *bus,
+    GError **error)
+{
+  GVariant *ret;
+  gchar **names;
+
+  g_assert (G_IS_DBUS_PROXY (bus));
+
+  ret = g_dbus_proxy_call_sync (bus, "ListNames", NULL,
+      G_DBUS_CALL_FLAGS_NONE, -1, NULL, error);
+  if (ret == NULL)
+    {
+      g_prefix_error (error, "Couldn't ListNames: ");
+      return FALSE;
+    }
+
+  for (g_variant_get_child (ret, 0, "^a&s", &names);
+       *names != NULL;
+       names++)
+    {
+      gchar *name = *names;
+
+      if (!g_dbus_is_unique_name (name) &&
+          strcmp (name, "org.freedesktop.DBus") != 0)
+        {
+          GVariant *owner = g_dbus_proxy_call_sync (bus, "GetNameOwner",
+              g_variant_new ("(s)", name),
+              G_DBUS_CALL_FLAGS_NONE, -1, NULL, NULL);
+
+          if (owner != NULL)
+            g_variant_unref (owner);
+          /* else they were too quick for us! */
+        }
+    }
+
+  g_variant_unref (ret);
+  return TRUE;
+}
+
+static gboolean
+initable_init (
+    GInitable *initable,
+    GCancellable *cancellable,
+    GError **error)
+{
+  BustlePcap *self = BUSTLE_PCAP (initable);
+  BustlePcapPrivate *priv = self->priv;
+  GDBusProxy *bus;
+
+  if (priv->bus_type == G_BUS_TYPE_NONE)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED,
+          "Logging things other than message busses is not supported");
+      return FALSE;
+    }
+
+  if (priv->filename == NULL)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_INVALID_ARGUMENT,
+          "You must specify a filename");
+      return FALSE;
+    }
+
+  /* FIXME: use DLT_DBUS when it makes it into libpcap. */
+  priv->p = pcap_open_dead (DLT_NULL, 1 << 27);
+  if (priv->p == NULL)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+          "pcap_open_dead failed. wtf");
+      return FALSE;
+    }
+
+  priv->td.dumper = pcap_dump_open (priv->p, priv->filename);
+  if (priv->td.dumper == NULL)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+          "Couldn't open pcap dump: %s", pcap_geterr (priv->p));
+      return FALSE;
+    }
+
+  priv->thread = g_thread_create (log_thread, &priv->td, TRUE, error);
+  if (priv->thread == NULL)
+    {
+      g_prefix_error (error, "Couldn't spawn logging thread: ");
+      return FALSE;
+    }
+
+  priv->connection = g_bus_get_sync (priv->bus_type, NULL, error);
+  if (priv->connection == NULL)
+    {
+      g_prefix_error (error, "Couldn't connect to %s bus: ",
+          priv->bus_type == G_BUS_TYPE_SESSION ? "session" : "system");
+      return FALSE;
+    }
+
+  priv->caps = g_dbus_connection_get_capabilities (priv->connection);
+
+  bus = g_dbus_proxy_new_sync (priv->connection,
+      G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES |
+      G_DBUS_PROXY_FLAGS_DO_NOT_CONNECT_SIGNALS,
+      NULL,
+      "org.freedesktop.DBus",
+      "/org/freedesktop/DBus",
+      "org.freedesktop.DBus",
+      NULL,
+      error);
+  if (bus == NULL)
+    {
+      g_prefix_error (error, "Couldn't construct bus proxy: ");
+      return FALSE;
+    }
+
+  if (!match_everything (bus, error))
+    return FALSE;
+
+  priv->filter_id = g_dbus_connection_add_filter (priv->connection, filter,
+      g_object_ref (self), g_object_unref);
+
+  {
+    /* FIXME: there's a race between listing all the names and binding to all
+     * signals (and hence getting NameOwnerChanged). Old bustle-dbus-monitor had
+     * it too.
+     */
+    gboolean ret = list_all_names (bus, error);
+    g_object_unref (bus);
+
+    return ret;
+  }
+}
+
+/* FIXME: make this async? */
+void
+bustle_pcap_stop (
+    BustlePcap *self)
+{
+  BustlePcapPrivate *priv = self->priv;
+
+  if (priv->filter_id != 0)
+    {
+      g_return_if_fail (priv->connection != NULL);
+      g_dbus_connection_remove_filter (priv->connection, priv->filter_id);
+      priv->filter_id = 0;
+    }
+
+  if (priv->connection != NULL &&
+      !g_dbus_connection_is_closed (priv->connection))
+    {
+      g_dbus_connection_close_sync (priv->connection, NULL, NULL);
+    }
+
+  if (priv->thread != NULL)
+    {
+      g_return_if_fail (priv->td.message_queue != NULL);
+      /* Wait for the writer thread to spit out all the messages, then close up. */
+      g_async_queue_push (priv->td.message_queue, STOP);
+      g_thread_join (priv->thread);
+      priv->thread = NULL;
+    }
+
+  if (priv->td.dumper != NULL)
+    {
+      pcap_dump_close (priv->td.dumper);
+      priv->td.dumper = NULL;
+    }
+
+  if (priv->p != NULL)
+    {
+      pcap_close (priv->p);
+      priv->p = NULL;
+    }
+}
+
+static void
+initable_iface_init (
+    gpointer g_class,
+    gpointer unused)
+{
+  GInitableIface *iface = g_class;
+
+  iface->init = initable_init;
+}
+
+BustlePcap *
+bustle_pcap_new (
+    GBusType bus_type,
+    const gchar *filename,
+    gboolean verbose,
+    GError **error)
+{
+  return g_initable_new (
+      BUSTLE_TYPE_PCAP, NULL, error,
+      "bus-type", bus_type,
+      "filename", filename,
+      "verbose", verbose,
+      NULL);
+}
